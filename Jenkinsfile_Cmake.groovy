48ed21e1144f788f78b3568bb32dbb1fdda670ff
LatifHosseini <113984497+LatifHosseini@users.noreply.github.com>
2023-04-17 22:56:59 +0200

    Update Jenkinsfile_Cmake.groovy

--- Jenkinsfile_Cmake.groovy
+++ Jenkinsfile_Cmake.groovy
@@ -1,39 +1,29 @@
-pipeline {
-    agent {label 'Agent'}
+ipeline {
+    agent { label 'Agent ' }
     tools {
         //maven 'mvn-default'
         //jdk 'jdk-default'
     }
+
     stages {
-        stage ('CHECKOUT') {
+        stage('checkout') {
             steps {
-                    cleanWs()
-                    checkout scm
+                echo 'Hello World'
+                cleanWs()
+                 checkout scm
             }
         }
         
-        stage ('Build') {
+        stage('Hello') {
             steps {
-               // sh '${M2_HOME}/bin/mvn --batch-mode -V -U -e clean verify -Dsurefire.useFile=false -Dmaven.test.failure.ignore'
+                echo 'Hello World'
             }
         }
-
-        stage ('Analysis') {
+        
+        stage('Hello') {
             steps {
-               // sh '${M2_HOME}/bin/mvn --batch-mode -V -U -e checkstyle:checkstyle pmd:pmd pmd:cpd findbugs:findbugs spotbugs:spotbugs'
+                echo 'Hello World'
             }
-        }
-    }
-    post {
-        always {
-           // junit testResults: '**/target/surefire-reports/TEST-*.xml'
-
-           // recordIssues enabledForFailure: true, tools: [mavenConsole(), java(), javaDoc()]
-            //recordIssues enabledForFailure: true, tool: checkStyle()
-            //recordIssues enabledForFailure: true, tool: spotBugs()
-           // recordIssues enabledForFailure: true, tool: cpd(pattern: '**/target/cpd.xml')
-           // recordIssues enabledForFailure: true, tool: pmdParser(pattern: '**/target/pmd.xml')
         }
     }
 }
-
